5daf1f61ce2a71d7db221d0b64827e492aadf8e7
SkiddingScripts <84696723+SkiddingScripts@users.noreply.github.com>
2021-05-28 01:53:46 -0500

    Update and rename TestAimlock.lua to TestAimlock1.lua

--- TestAimlock1.lua
+++ TestAimlock1.lua
@@ -244,11 +244,8 @@ function ValiantAimHacks.UntilDead()
 local ClosestBruh = ValiantAimHacks.getClosestPlayerToCursor()
 local chance, target, LocalPlayer = ValiantAimHacks.getClosestPlayerToCursor()
 -- print("Made it to line 246 probably target is nil??")
-if target ~= nil then
-print("Made it to line 247")
 if target.Character.BodyEffects["K.O"].Value == false then
 ValiantAimHacks.Selected = (chance and target or LocalPlayer)
-end
 end
 end
 
