eccce5f4b7d64d84b46537f02701459e7af1bdb7
SkiddingScripts <84696723+SkiddingScripts@users.noreply.github.com>
2021-05-29 04:40:23 -0500

    Update and rename TestAimlock.lua to TestAimlock1.lua

--- TestAimlock1.lua
+++ TestAimlock1.lua
@@ -270,11 +270,11 @@ end
 
 function ValiantAimHacks.TargetPlayer()
 local Player = ValiantAimHacks.Selected
-if Player ~= nil and not LocalPlayer and ValiantAimHacks.isRadius(Player) and Player.Character.BodyEffects["K.O"].Value == false then
+if Player ~= nil and not LocalPlayer and ValiantAimHacks.isRadius() and Player.Character.BodyEffects["K.O"].Value == false then
+print(tostring(ValiantAimHacks.isRadius()))
 ValiantAimHacks.Selected = Player
 print(ValiantAimHacks.Selected.Name)
 else
-print("Attacking Other player!!")
 ValiantAimHacks.getClosestPlayerToCursor()
 end
 end
