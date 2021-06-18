d3fb6ff4aa22611a9174bedf3252f0bcb74df7e8
SkiddingScripts <84696723+SkiddingScripts@users.noreply.github.com>
2021-06-17 19:41:19 -0500

    Update and rename TestAimlock.lua to TestAimlock1.lua

--- TestAimlock1.lua
+++ TestAimlock1.lua
@@ -298,7 +298,7 @@ else
 if Selected ~= nil then
 --local Character = ValiantAimHacks.getCharacter(Selected)
 --local TargetPart = Character[ValiantAimHacks.TargetPart]
-if Selected ~= nil and Selected.Character:WaitForChild("BodyEffects") ~= nil and Selected ~= LocalPlayer and Selected.Character.BodyEffects["K.O"].Value == false and ValiantAimHacks.Radius(Selected) then
+if Selected ~= nil and Selected.Character:WaitForChild("BodyEffects") ~= nil and Selected ~= LocalPlayer and Selected.Character.BodyEffects["K.O"].Value == false and ValiantAimHacks.Radius(Selected) and ValiantAimHacks.Visible(Selected) then
 ValiantAimHacks.Selected = (Selected or LocalPlayer)
 else
 ValiantAimHacks.getClosestPlayerToCursor()
