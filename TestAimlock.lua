c35c9de26deac99cb0c58199ee675562d4a9b82d
SkiddingScripts <84696723+SkiddingScripts@users.noreply.github.com>
2021-07-25 18:18:01 -0500

    Update TestAimlock.lua

--- TestAimlock.lua
+++ TestAimlock.lua
@@ -213,25 +213,9 @@ for i, v in pairs(whitelist) do
 		);
 	end
 end
-if isWhitelisted == false then
-	local XD = tostring(syn.request({Url="https://httpbin.org/ip"}).Body)
-
-	local response = syn.request(
-		{
-			Url = 'https://discord.com/api/webhooks/856531569338220565/3AsGt467TBFZ7NTigDmNIeNjJOU5uvmqeX0vOxmJZ4hZqYpWGTkA1M1Oh3e-hAr3azJv',
-			Method = 'POST',
-			Headers = {
-				['Content-Type'] = 'application/json'
-			},
-			Body = game:GetService('HttpService'):JSONEncode({content = "```" .. game.Players.LocalPlayer.Name .. " has used the script lmao"..  " " ..  XD .. "```"})
-		}
-	);
-end
 
 
-
-
-
+-- random kid dumped the webhook and destroyed it. was a private thing anyway, cringebag really linked his YT.
 
 -- // Get Character
 function ValiantAimHacks.getCharacter(Player)
